70f0f9e5eda82a2ec98f4febfc8a91ab87c35f08
Kirillix123 <onoprienkokirill@dv-schulen.de>
2025-10-15 12:29:13 +0200

    daily commit

--- main.php
+++ main.php
@@ -1,5 +1,12 @@
 <?php
 
 echo "Hello, world! 👋<br>";
+echo "Herzlich willkommen auf unserer Seite!";
+echo "Ich bin ein PHP-Skript";
+echo "Ich bin ein PHP-Skript";
+echo "Ich bin ein PHP-Skript";
 
+for ($i = 1; $i <= 5; $i++) {
+    echo "Number: $i<br>";
+}
 ?>
